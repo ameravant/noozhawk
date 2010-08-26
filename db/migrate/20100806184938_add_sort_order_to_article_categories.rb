d6a755567d4c3576ebfd9029d7f4a59bcb3d1865
Ameravant <contact@ameravant.com>
2010-08-26 10:43:32 -0700

    modified populate_nooz rake

--- db/migrate/20100806184938_add_sort_order_to_article_categories.rb
+++
@@ -1,9 +0,0 @@
-class AddSortOrderToArticleCategories < ActiveRecord::Migration
-  def self.up
-    add_column :article_categories, :sort_order, :integer
-  end
-
-  def self.down
-    remove_column :article_categories, :sort_order
-  end
-end
