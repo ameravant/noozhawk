3a1563082422783f4267567037f4c0ca8e1a1b34
jason gagne <jgagne33@gmail.com>
2010-10-13 12:58:34 -0700

    adding routes for features and images

--- routes.rb
+++ routes.rb
@@ -1,6 +1,7 @@
 resources :newsgrids, :only => :show
 namespace :admin do |admin|
-  admin.resources :newsgrids do |newsgrid|
+  admin.resources :newsgrids, :has_many => { :features, :menus } do |newsgrid|
     newsgrid.resources :menus
+    newsgrid.resources :images, :member => { :reorder => :put }, :collection => { :reorder => :put }
   end
 end
